0009f6302b1b9b8cbee6373333dc605826410d76
Troy Alford <troy@troyalford.com>
2017-06-10 22:00:58 -0700

    Adding Immutable.Map reference, plus better tests

--- src/utility/renderStyled.js
+++ src/utility/renderStyled.js
@@ -1,19 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope,react/prop-types */
 import React from 'react'
+import { Map } from 'immutable'
 
 export default (Tag, { data = new Map(), children = [], attributes = {} } = {}) => {
   if (!Tag || typeof Tag !== 'string') throw new Error('Tag is required')
 
-  let dataMap;
-  if (data instanceof Map)
-    dataMap = data
-  else if (typeof data === 'object')
-    dataMap = new Map(Object.entries(data))
-  else
-    dataMap = new Map()
-  // const dataMap = data instanceof Map ? data :
-  //                 typeof data === 'object' ? new Map(Object.entries(data)) :
-  //                 new Map()
+  const dataMap = data instanceof Map ? data :
+                  typeof data === 'object' ? new Map(data) :
+                  new Map()
 
   const style = dataMap.get('style') || {}
   const className = dataMap.get('className') || undefined
--- src/utility/renderStyled.test.js
+++ src/utility/renderStyled.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Map } from 'immutable'
 import renderStyled from './renderStyled'
 
 jest.unmock('./renderStyled')
