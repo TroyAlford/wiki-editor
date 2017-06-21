4cd8a36afcbe2e90cab2971a5f4091efdff74870
Troy Alford <troy@troyalford.com>
2017-06-20 19:16:30 -0700

    :bug:fix - table cells should be created with a <p> in them, not a text node

--- src/plugins/Table/Creators.js
+++ src/plugins/Table/Creators.js
@@ -1,13 +1,9 @@
 import Slate from 'slate'
+import Paragraph from '../Paragraph'
 import { range } from '../../utility/range'
 
 export function createCell() {
-  const textNode = Slate.Raw.deserializeText({
-    kind: 'text',
-    text: '',
-  }, { terse: true })
-
-  return Slate.Block.create({ type: 'td', nodes: [textNode] })
+  return Slate.Block.create({ type: 'td', nodes: [Paragraph.create()] })
 }
 export function createRow(columns = 2) {
   return Slate.Block.create({
