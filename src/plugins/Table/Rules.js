467d1f0d61665f2d50eca3b7d9d13358d75b6e13
Troy Alford <troy@troyalford.com>
2017-06-20 19:48:26 -0700

    Improved auto-correction of table nesting

--- src/plugins/Table/Rules.js
+++ src/plugins/Table/Rules.js
@@ -1,8 +1,11 @@
 import { contains } from '../../utility/contains'
+import { flow } from '../../utility/flow'
 
 const PARENTS = {
   table: ['tr', 'thead', 'tbody', 'tfoot'],
   tr:    ['td', 'th'],
+  th:    ['paragraph'],
+  td:    ['paragraph'],
 }
 const PARENT_KEYS = Object.keys(PARENTS)
 
@@ -17,7 +20,8 @@ const DEFAULT_PARENT = CHILD_KEYS.reduce(
   (o, key) => ({ ...o, [key]: CHILDREN[key][0] })
 , {})
 
-const wrapInvalidTableChildren = {
+// Rule to ensure that tables are nested TABLE > TBODY > TR > TD|TH
+const enforceTableParentingRules = {
   match: ({ type }) => contains(PARENT_KEYS, type),
 
   validate: (parent) => {
@@ -28,27 +32,46 @@ const wrapInvalidTableChildren = {
   },
 
   normalize(transform, node, children) {
-    return children.reduce((t, child) => {
-      const parentTag = DEFAULT_PARENT[child.type] || 'td'
-      return t.wrapBlockByKey(child.key, parentTag, { normalize: false })
-    }, transform)
+    const doc = transform.state.document
+    const ancestors = doc.getAncestors(node.key)
+    const parent = ancestors.get(0)
+
+    return flow([
+      // Wrap this element, if needed, in an appropriate parent
+      t => {
+        if (parent && contains(CHILDREN[node.type], parent.type)) return t
+        return t.wrapBlockByKey(node.key, DEFAULT_PARENT[node.type], { normalize: false })
+      },
+
+      // Wrap immediate children in the appropriate parent
+      t => children.reduce((inner, child) => {
+        const parentTag = DEFAULT_PARENT[child.type] || 'td'
+        return inner.wrapBlockByKey(child.key, parentTag, { normalize: false })
+      }, t)
+    ], transform)
+
   },
 }
 
-// const wrapInvalidTableOrphans = {
-//   match:     ({ type }) => contains(CHILD_KEYS, type),
-//   validate:  () => true,
-//   normalize: (transform, child) => {
-//     const { state } = transform
-//     const parent = state.document.getParent(child.key)
-//     if (contains(CHILDREN[child.type], parent.type)) return undefined
+// Rule to ensure that the contents of TD and TH cells are wrapped in a <p>
+const wrapCellContents = {
+  match: ({ type }) => contains(['th', 'td'], type),
 
-//     // this node is an orphaned table child. replace with <p>
-//     return transform.setNodeByKey(child.key, { type: 'paragraph' })
-//   },
-// }
+  validate: (parent) => {
+    const textNodes = parent.nodes.filter(
+      child => child.kind === 'text'
+    )
+    return !textNodes.isEmpty() ? textNodes : undefined
+  },
+
+  normalize(transform, node, children) {
+    return children.reduce((t, child) =>
+      t.wrapBlockByKey(child.key, 'paragraph', { normalize: false })
+    , transform)
+  }
+}
 
 export default [
-  wrapInvalidTableChildren,
-  // wrapInvalidTableOrphans,
+  wrapCellContents,
+  enforceTableParentingRules,
 ]
