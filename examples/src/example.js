ce8191f5538bc289a7866df55ef36d4ecd30ec7b
Troy Alford <troy@troyalford.com>
2017-06-20 22:33:53 -0700

    Removing extraneous trailing slashes

--- examples/src/example.js
+++ examples/src/example.js
@@ -7,19 +7,19 @@ window.React = React
 window.ReactDOM = ReactDOM
 
 const HTML = `
-<h1 class="header-class" style="color: red;">Header 1</h1>\
-<p>This is some stuff with a [test](text) link put in the middle.</p>\
-<table class="blah">\
-  <tr>\
-    <th colspan="2">Test</th>\
-  </tr>\
-  <tr>\
-    <td>Left</td><td>Right</td>\
-  </tr>\
-</table>\
-<p><a href="foo">Bar</a></p>\
-<p><a href="with" class="class names">Test</a></p>\
-<p><img src="http://lorempixel.com/100/100/" /></p>\
+<h1 class="header-class" style="color: red;">Header 1</h1>
+<p>This is some stuff with a [test](text) link put in the middle.</p>
+<table class="blah">
+  <tr>
+    <th colspan="2">Test</th>
+  </tr>
+  <tr>
+    <td>Left</td><td>Right</td>
+  </tr>
+</table>
+<p><a href="foo">Bar</a></p>
+<p><a href="with" class="class names">Test</a></p>
+<p><img src="http://lorempixel.com/100/100/" /></p>
 `
 
 class Example extends React.Component {
