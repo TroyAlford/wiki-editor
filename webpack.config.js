fe8b4b5f972db1fa89c682ceef3ab5ced7e16a37
Troy Alford <troy@troyalford.com>
2017-11-03 15:49:40 -0700

    Updating dependencies and build tools

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path')
 const webpack = require('webpack')
 
-const ENVIRONMENT = process.env.NODE_ENV
+const ENVIRONMENT = process.env.NODE_ENV || 'development'
 const PRODUCTION = ENVIRONMENT === 'production'
 
 const library = 'wiki-editor'
@@ -23,6 +23,10 @@ const bundle = {
       test:    /\.js$/,
       loader:  'babel-loader',
       exclude: /node_modules/,
+      query: {
+        plugins: ['transform-object-rest-spread', 'transform-class-properties'],
+        presets: ['env', 'react'],
+      },
     }],
   },
   output: {
@@ -32,15 +36,20 @@ const bundle = {
     libraryTarget:  'umd',
     umdNamedDefine: true,
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(ENVIRONMENT),
+    }),
+  ]
 }
 
-if (PRODUCTION) bundle.plugins = [
-  new webpack.optimize.UglifyJsPlugin({ minimize: true }),
-  new webpack.optimize.ModuleConcatenationPlugin(),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(ENVIRONMENT),
+if (PRODUCTION) bundle.plugins.push(
+  new webpack.LoaderOptionsPlugin({
+    minimize: true,
+    debug: false,
   }),
-]
+  new webpack.optimize.ModuleConcatenationPlugin()
+)
 
 module.exports = [
   Object.assign({}, bundle, {
