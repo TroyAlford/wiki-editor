b7205db8d25a232de75c7c1d4b86bd3baf49517a
Troy Alford <troy@troyalford.com>
2017-12-28 22:01:11 -0800

    Update parseStyle utility to handle Node of nodeType 2 (style attribute)

--- source/utility/parseStyle.js
+++ source/utility/parseStyle.js
@@ -1,22 +1,22 @@
 import camelCase from './camelCase'
 
 export default function parseStyle(style) {
-  switch (typeof style) {
-    case 'string':
-      return style.split(';').filter(r => r)
-        .reduce((map, rule) => {
-          const name = rule.slice(0, rule.indexOf(':')).trim()
-          const value = rule.slice(rule.indexOf(':') + 1).trim()
+  if (typeof style === 'string') {
+    return style.split(';').filter(r => r)
+      .reduce((map, rule) => {
+        const name = rule.slice(0, rule.indexOf(':')).trim()
+        const value = rule.slice(rule.indexOf(':') + 1).trim()
 
-          return {
-            ...map,
-            [camelCase(name)]: value,
-          }
-        }, {})
-    case 'object':
-      return style
-
-    default:
-      return undefined
+        return {
+          ...map,
+          [camelCase(name)]: value,
+        }
+      }, {})
+  } else if (style instanceof Node && style.nodeType === 2) {
+    return parseStyle(style.textContent)
+  } else if (typeof style === 'object') {
+    return style
   }
+
+  return undefined
 }
